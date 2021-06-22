31ecbf872aa543955c2e3f2f255c5c19ce87d08a
Adam Rogers <adam@adamrogers.ca>
2021-06-21 23:08:44 -0400

    add basic show list styles

--- src/components/BasicShowListExample/BasicShowListExample.js
+++ src/components/BasicShowListExample/BasicShowListExample.js
@@ -11,15 +11,17 @@ const BasicShowListExample = () => {
   
   const handleAddClick = () => {
     setShows([...shows, inputValue]);
+    setInputValue('');
   }
 
   return (
-    <div>
+    <div class="shows">
       <div>
-        <input value={inputValue} onChange={handleInputChange} />
+        <input className="shows-input" value={inputValue} onChange={handleInputChange} />
         <button onClick={handleAddClick}>Add Show</button>
       </div>
-      <ul>
+      {shows.length > 5 && <p>We're over five shows? ALL HAIL TELEVISION</p>}
+      <ul class="shows-list">
         {shows.map(show => <li key={show}>{show}</li>)}
       </ul>
     </div>
